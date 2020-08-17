eb65fd142df9d085eda1e0de1a5efc8ed58f5862
darkknight77 <shaiklatheefmrf98@gmail.com>
2020-08-17 14:51:14 +0530

    String InterPolation + PropertyBinding + Event Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,4 +5,11 @@ selector : 'app-server',
 templateUrl : './server.component.html'
 
 })
-export class ServerComponent {}
+export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
+getServerStatus(){
+    return this.serverStatus;
+}
+
+}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,15 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // selector: '[app-servers]',
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>'
-  
+ // template: '<app-server></app-server><app-server></app-server>'
+  templateUrl: './servers.component.html'
 })
 
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+ 
+  isServerAllowed = false;
+  serverCreationStatus = "No server is created";
+  constructor() { 
+    setTimeout(() => {
+      this.isServerAllowed = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
+
+  onServerCreate(){
+this.serverCreationStatus = 'A new server is created';
+
+  };
+
+
 
 }
